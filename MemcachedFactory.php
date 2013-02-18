d6a26a1e0763be89142aad5047d7159822cb8c5e
Pierre Durand <pierre@plyce.com>
2013-02-18 14:57:19 +0100

    More unit tests

--- MemcachedFactory.php
+++ MemcachedFactory.php
@@ -9,20 +9,43 @@ class MemcachedFactory
         $persistentId = isset($config['persistent_id']) ? $config['persistent_id'] : null;
         $memcached = new \Memcached($persistentId);
 
-        if (count($memcached->getServerList()) == 0) { //Re use persistent instance
-            if (isset($config['servers'])) {
-                foreach ($config['servers'] as $server) {
-                    $memcached->addServer($server['host'], $server['port'], isset($server['weight']) ? $server['weight'] : null);
-                }
-            }
-
-            if (isset($config['options'])) {
-                foreach ($config['options'] as $optionName => $optionValue) {
-                    $memcached->setOption($optionName, $optionValue);
-                }
-            }
+        if ($this->isInitializationRequired($memcached)) { //Re use persistent instance
+            $this->initialize($memcached, $config);
         }
 
         return $memcached;
+    }
+
+    private function isInitializationRequired(\Memcached $memcached)
+    {
+        return count($memcached->getServerList()) == 0;
+    }
+
+    private function initialize(\Memcached $memcached, array $config)
+    {
+        $this->addServers($memcached, $config);
+
+        $this->setOptions($memcached, $config);
+    }
+
+    private function addServers(\Memcached $memcached, array $config)
+    {
+        if (isset($config['servers'])) {
+            foreach ($config['servers'] as $serverConfig) {
+                $this->addServer($memcached, $serverConfig);
+            }
+        }
+    }
+
+    private function addServer(\Memcached $memcached, array $serverConfig)
+    {
+        $memcached->addServer($serverConfig['host'], $serverConfig['port'], isset($serverConfig['weight']) ? $serverConfig['weight'] : null);
+    }
+
+    private function setOptions(\Memcached $memcached, array $config)
+    {
+        if (isset($config['options'])) {
+            $memcached->setOptions($config['options']);
+        }
     }
 }
--- Tests/MemcachedFactoryTest.php
+++ Tests/MemcachedFactoryTest.php
@@ -8,11 +8,54 @@ class MemcachedFactoryTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @covers \Pierrre\MemcachedBundle\MemcachedFactory::get
+     * @covers \Pierrre\MemcachedBundle\MemcachedFactory::isInitializationRequired
+     * @covers \Pierrre\MemcachedBundle\MemcachedFactory::initialize
+     * @covers \Pierrre\MemcachedBundle\MemcachedFactory::addServers
+     * @covers \Pierrre\MemcachedBundle\MemcachedFactory::addServer
+     * @covers \Pierrre\MemcachedBundle\MemcachedFactory::setOptions
      */
-    public function testGetInstanceOf()
+    public function testGet()
     {
         $memcachedFactory = new MemcachedFactory();
-        $memcached = $memcachedFactory->get(array());
+        $memcached = $memcachedFactory->get(array(
+            'servers' => array(
+                array(
+                    'host' => 'localhost',
+                    'port' => '11211'
+                )
+            ),
+            'options' => array(
+                \Memcached::OPT_COMPRESSION => true
+            )
+        ));
         $this->assertInstanceOf('Memcached', $memcached);
+    }
+
+    /**
+     * @coversNothing
+     */
+    public function testPersistentConnection()
+    {
+        $memcachedFactory = new MemcachedFactory();
+
+        $config = array(
+            'persistent_id' => 'test',
+            'servers' => array(
+                array(
+                    'host' => 'localhost',
+                    'port' => '11211'
+                )
+            )
+        );
+
+        $memcached = $memcachedFactory->get($config);
+        $totalConnectionsStart = array_pop($memcached->getStats())['total_connections'];
+
+        for ($i = 0; $i < 10; $i++) {
+            $memcachedFactory->get($config);
+        }
+        $totalConnectionsEnd = array_pop($memcached->getStats())['total_connections'];
+
+        $this->assertEquals($totalConnectionsStart, $totalConnectionsEnd);
     }
 }
