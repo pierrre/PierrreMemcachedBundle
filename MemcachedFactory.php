8eeed78ddd5f65751d5c8e3fbfbfc3b75dc9e9c5
Pierre Durand <pierre@plyce.com>
2013-02-18 12:45:29 +0100

    Fix config

--- MemcachedFactory.php
+++ MemcachedFactory.php
@@ -10,12 +10,16 @@ class MemcachedFactory
         $memcached = new \Memcached($persistentId);
 
         if (count($memcached->getServerList()) == 0) { //Re use persistent instance
-            foreach ($config['servers'] as $server) {
-                $memcached->addServer($server['host'], $server['port'], $server['weight']);
+            if (isset($config['servers'])) {
+                foreach ($config['servers'] as $server) {
+                    $memcached->addServer($server['host'], $server['port'], isset($server['weight']) ? $server['weight'] : null);
+                }
             }
 
-            foreach ($config['options'] as $optionName => $optionValue) {
-                $memcached->setOption($optionName, $optionValue);
+            if (isset($config['options'])) {
+                foreach ($config['options'] as $optionName => $optionValue) {
+                    $memcached->setOption($optionName, $optionValue);
+                }
             }
         }
 
